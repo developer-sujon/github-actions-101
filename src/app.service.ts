8c2e16817e130de3e945d3c49ddb2d91c0f1e090
Muhammad Sujon <65336862+developer-sujon@users.noreply.github.com>
2024-03-28 03:33:23 +0600

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Multiple Self Hosted Servers!';
+    return 'Hello World Multiple Self Hosted Server!';
   }
 }
